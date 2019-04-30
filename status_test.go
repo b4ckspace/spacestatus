d8d0247082c070ea252eaee6f1863410be3e3c38
Stefan Schwarz <stefan@f2o.io>
2019-04-30 07:40:03 +0200

    add tests

---
+++ status_test.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strings"
+	"testing"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/google/go-cmp/cmp"
+	log "github.com/sirupsen/logrus"
+)
+
+func TestMqtt(t *testing.T) {
+	err := configure()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m := mqtt.NewClient(&mqtt.ClientOptions{
+		Servers:          []*url.URL{c.server},
+		ClientID:         "go-mqtt-spacestatus-2",
+		AutoReconnect:    true,
+		OnConnect:        func(c mqtt.Client) { log.Info("connected") },
+		OnConnectionLost: func(c mqtt.Client, err error) { log.Errorf("connection lost: %v", err) },
+	})
+	token := m.Connect()
+	log.Infof("%+v", token)
+
+	go main()
+
+	_ = m.Publish("sensor/space/member/names", 0, false, "a, b, c, d")
+	_ = m.Publish("sensor/space/member/count", 0, false, "30")
+	_ = m.Publish("sensor/temperature/lounge/podest", 0, false, "22.3")
+	_ = m.Publish("sensor/temperature/lounge/podest", 0, false, "23.3")
+	_ = m.Publish("sensor/temperature/lounge/ceiling", 0, false, "24.3")
+	_ = m.Publish("sensor/temperature/hackcenter/shelf", 0, false, "21.3")
+	_ = m.Publish("sensor/power/main/L1", 0, false, "123")
+	_ = m.Publish("sensor/power/main/L2", 0, false, "234")
+	_ = m.Publish("sensor/power/main/L3", 0, false, "345")
+	_ = m.Publish("sensor/power/main/total", 0, false, "1234")
+	_ = m.Publish("sensor/space/status", 0, false, "closed")
+	_ = m.Publish("sensor/space/member/deviceCount", 0, false, "77")
+
+	resp, err := http.Get("http://localhost:8080/")
+	if err != nil {
+		t.Errorf("Unable to query API: %v", err)
+	}
+
+	have, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Errorf("Unable to load body: %v", err)
+	}
+
+	want, err := ioutil.ReadFile("status_test.json")
+	if err != nil {
+		t.Errorf("Unable to load want: %v", err)
+	}
+
+	wants := strings.Split(string(want), "\n")
+	haves := strings.Split(string(have), "\n")
+	if diff := cmp.Diff(wants, haves); diff != "" {
+		t.Errorf("Invalid output. \n%s", diff)
+	}
+}
