63af8323e99e9932fb825c3b3bd7187d2e1b6ed5
Stefan Schwarz <stefan@f2o.io>
2020-01-08 08:11:45 +0100

    move server to own package

---
+++ server/server.go
@@ -0,0 +1,110 @@
+package server
+
+import (
+	"net/http"
+	"net/url"
+	"sync"
+	"text/template"
+	"time"
+
+	mqtt "github.com/eclipse/paho.mqtt.golang"
+	"github.com/kelseyhightower/envconfig"
+	log "github.com/sirupsen/logrus"
+
+	"github.com/b4ckspace/spacestatus/filters"
+	"github.com/b4ckspace/spacestatus/metrics"
+)
+
+type Server struct {
+	MqttURL *url.URL `envconfig:"MQTT_URL"`
+	Debug   bool     `envconfig:"DEBUG"`
+
+	Cache *sync.Map
+
+	mux      *http.ServeMux
+	template *template.Template
+}
+
+func NewServer() (s *Server, err error) {
+	s = &Server{}
+	s.mux = http.NewServeMux()
+	err = envconfig.Process("", s)
+	if err != nil {
+		return nil, err
+	}
+	if s.Debug {
+		log.SetLevel(log.DebugLevel)
+	}
+	return s, nil
+}
+
+// ConnectMqtt connects to mqtt
+func (s *Server) ConnectMqtt() (err error) {
+	m := mqtt.NewClient(&mqtt.ClientOptions{
+		Servers:       []*url.URL{s.MqttURL},
+		ClientID:      "go-mqtt-spacestatus-dev",
+		AutoReconnect: true,
+		OnConnect: func(c mqtt.Client) {
+			metrics.Count("spacestatus_mqtt{state=\"connected\"}")
+			log.Infof("connected")
+		},
+		OnConnectionLost: func(c mqtt.Client, err error) {
+			metrics.Count("spacestatus_mqtt{state=\"disconnected\"}")
+			log.WithError(err).Errorf("connection lost")
+		},
+	})
+	t := m.Connect()
+	_ = t.Wait()
+	if err := t.Error(); err != nil {
+		return err
+	}
+	t = m.Subscribe("#", 0, func(c mqtt.Client, m mqtt.Message) {
+		metrics.Count("spacestatus_mqtt{state=\"message\"}")
+		log.Debugf("%s: %s", m.Topic(), string(m.Payload()))
+		s.Cache.Store(m.Topic(), string(m.Payload()))
+	})
+	t.Wait()
+	if err := t.Error(); err != nil {
+		return err
+	}
+	log.Println("subscribed")
+	return
+}
+
+// LoadTemplates loads the template filters and files
+func (s *Server) LoadTemplates() (err error) {
+	s.template, err = template.New("base").Funcs(template.FuncMap{
+		"mqtt":    filters.MqttLoadForCache(s.Cache),
+		"csvlist": filters.CsvList,
+		"jsonize": filters.Jsonize,
+	}).ParseFiles("status-template.json")
+	return
+}
+
+// Serve handles http
+func (s *Server) ListenAndServe() (err error) {
+	s.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		metrics.Count("spacestatus_requests")
+		w.Header().Add("content-type", "application/json")
+		err := s.template.ExecuteTemplate(w, "status-template.json", nil)
+		if err != nil {
+			log.WithError(err).Infof("unable to render template")
+		}
+	})
+	s.mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
+	middleware := func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			start := time.Now()
+			next.ServeHTTP(w, r)
+			log.WithFields(log.Fields{
+				"duration": time.Since(start).String(),
+				"method":   r.Method,
+			}).Info(r.RequestURI)
+		})
+	}
+	return http.ListenAndServe(":8080", middleware(s.mux))
+}
+
+func (s *Server) GetMux() *http.ServeMux {
+	return s.mux
+}
--- status.go
+++ status.go
@@ -1,120 +1,37 @@
 package main
 
 import (
-	"net/http"
-	"net/url"
-	"sync"
-	"text/template"
-	"time"
-
-	mqtt "github.com/eclipse/paho.mqtt.golang"
-	"github.com/kelseyhightower/envconfig"
 	log "github.com/sirupsen/logrus"
 
-	"github.com/b4ckspace/spacestatus/filters"
 	"github.com/b4ckspace/spacestatus/metrics"
+	"github.com/b4ckspace/spacestatus/server"
 )
 
-type server struct {
-	MqttURL *url.URL `envconfig:"MQTT_URL"`
-	Debug   bool     `envconfig:"DEBUG"`
-
-	Cache *sync.Map
-
-	mux      *http.ServeMux
-	template *template.Template
-}
-
 func main() {
-	s := &server{}
-	s.mux = http.NewServeMux()
-	err := envconfig.Process("", s)
+	// server
+	s, err := server.NewServer()
 	if err != nil {
 		log.WithError(err).Fatalf("unable to process env")
 	}
-	if s.Debug {
-		log.SetLevel(log.DebugLevel)
-	}
 
 	// mqtt
-	err = s.connectMqtt()
+	err = s.ConnectMqtt()
 	if err != nil {
 		log.WithError(err).Fatalf("unable to connect mqtt")
 	}
 
 	// template
-	err = s.loadTemplates()
+	err = s.LoadTemplates()
 	if err != nil {
 		log.WithError(err).Fatalf("unable to load templates")
 	}
 
 	// metrics
-	metrics.Register(s.mux)
+	metrics.Register(s.GetMux())
 
 	// serve http
-	s.serve()
-}
-
-func (s *server) connectMqtt() (err error) {
-	m := mqtt.NewClient(&mqtt.ClientOptions{
-		Servers:       []*url.URL{s.MqttURL},
-		ClientID:      "go-mqtt-spacestatus-dev",
-		AutoReconnect: true,
-		OnConnect: func(c mqtt.Client) {
-			metrics.Count("spacestatus_mqtt{state=\"connected\"}")
-			log.Infof("connected")
-		},
-		OnConnectionLost: func(c mqtt.Client, err error) {
-			metrics.Count("spacestatus_mqtt{state=\"disconnected\"}")
-			log.WithError(err).Errorf("connection lost")
-		},
-	})
-	t := m.Connect()
-	_ = t.Wait()
-	if err := t.Error(); err != nil {
-		return err
+	err = s.ListenAndServe()
+	if err != nil {
+		log.WithError(err).Fatalf("unable to listen")
 	}
-	t = m.Subscribe("#", 0, func(c mqtt.Client, m mqtt.Message) {
-		metrics.Count("spacestatus_mqtt{state=\"message\"}")
-		log.Debugf("%s: %s", m.Topic(), string(m.Payload()))
-		s.Cache.Store(m.Topic(), string(m.Payload()))
-	})
-	t.Wait()
-	if err := t.Error(); err != nil {
-		return err
-	}
-	log.Println("subscribed")
-	return
-}
-
-func (s *server) loadTemplates() (err error) {
-	s.template, err = template.New("base").Funcs(template.FuncMap{
-		"mqtt":    filters.MqttLoadForCache(s.Cache),
-		"csvlist": filters.CsvList,
-		"jsonize": filters.Jsonize,
-	}).ParseFiles("status-template.json")
-	return
-}
-
-func (s *server) serve() (err error) {
-	s.mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		metrics.Count("spacestatus_requests")
-		w.Header().Add("content-type", "application/json")
-		err := s.template.ExecuteTemplate(w, "status-template.json", nil)
-		if err != nil {
-			log.WithError(err).Infof("unable to render template")
-		}
-	})
-	s.mux.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {})
-	middleware := func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			start := time.Now()
-			next.ServeHTTP(w, r)
-			log.WithFields(log.Fields{
-				"duration": time.Since(start).String(),
-				"method":   r.Method,
-			}).Info(r.RequestURI)
-		})
-	}
-	return http.ListenAndServe(":8080", middleware(s.mux))
 }
